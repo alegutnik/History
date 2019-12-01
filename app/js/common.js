c61d4377f6c079095973fc0adefd8d6c5c4f405c
Davyd Tevzadze <pawtod20@gmail.com>
2019-12-01 02:26:08 +0200

    no message

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,16 @@
 $(function() {
 
-	// Custom JS
 
+		$('.card_inactive').find('.quest_menu_inactive').click(function(){
+			$(this).prev('.quest_block').children('.quest_answer').slideDown();
+			$(this).html('<i class="fas fa-times-circle"></i>');
+			$(this).removeClass('quest_menu_inactive');
+			$(this).addClass('quest_menu_active');
+			$(this).parent('.card_inactive').addClass('card');
+		});
+
+		$('.quest_menu_active').click(function(){
+			$(this).prev('.quest_block').children('.quest_answer').slideUp();
+		})
+	
 });
